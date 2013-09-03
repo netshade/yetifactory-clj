ab968eb88778efdc31380703fdd5ecd3f521e0ab
Chris <christopher@fastestforward.com>
2013-09-02 22:05:04 -0400

    use correct location

--- src/yetifactory/core.clj
+++ src/yetifactory/core.clj
@@ -1,6 +1,7 @@
 (ns yetifactory.core
   (:use [clojure.stacktrace])
   (:require [yetifactory.adapter :as adapter])
+  (:require [yetifactory.router :as router])
   (:require [ring.middleware.file :as file])
   (:require [ring.middleware.file-info :as file-info])
   (:require [yetifactory.template :as template])
@@ -10,6 +11,6 @@
   (adapter/run-server
       (file-info/wrap-file-info
         (file/wrap-file
-          (template/wrap-template router "./templates")
+          (template/wrap-template router/route "./templates")
         "./public"))
     { :port 5000 }))
