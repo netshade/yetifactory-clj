a3e0dd18286c4947d1ac5590e5fadaae3ef8ff11
Chris <christopher@fastestforward.com>
2013-09-07 21:43:13 -0400

    break apart main html into sub templates, remove markdown dependency, add relative time, pass along relative time in post maps

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,10 @@ USE yetifactory;
 CREATE TABLE IF NOT EXISTS posts (
   id          INT(11) NOT NULL AUTO_INCREMENT,
   title       VARCHAR(255) NOT NULL,
+  body        TEXT,
   body_md     TEXT,
+  snippet     TEXT,
+  snippet_md  TEXT,
   created_at  DATETIME NOT NULL,
   updated_at  DATETIME NOT NULL,
   PRIMARY KEY(id)
--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Basic example of using netty with Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [io.netty/netty-all "4.0.8.Final"]
-                 [markdown-clj "0.9.31"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-devel "1.2.0"]
                  [org.antlr/ST4 "4.0.7"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
                  [mysql/mysql-connector-java "5.1.6"]
-                 [com.mchange/c3p0 "0.9.2.1"]]
+                 [com.mchange/c3p0 "0.9.2.1"]
+                 [org.ocpsoft.prettytime/prettytime "3.0.2.Final"]]
   :java-source-paths ["src/main/java"]
   :main yetifactory.core)
