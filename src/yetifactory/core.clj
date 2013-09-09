9b56eba21433b3677d0182770475fc972b7f2564
Chris <christopher@fastestforward.com>
2013-09-08 21:42:59 -0400

    port specified via defaults

--- src/yetifactory/core.clj
+++ src/yetifactory/core.clj
@@ -1,14 +1,15 @@
 (ns yetifactory.core
   (:use [clojure.stacktrace])
+  (:use [environ.core])
   (:require [yetifactory.adapter :as adapter])
   (:require [yetifactory.router :as router])
   (:require [ring.middleware.file :as file])
+  (:require [clojure.java.io])
   (:require [ring.middleware.file-info :as file-info])
   (:require [yetifactory.template :as template])
   (:require [yetifactory.header-defaults :as header-defaults])
   (:require [clojure.pprint :as pprint])
   (:gen-class))
-
 
 (defn -main []
   (adapter/run-server
@@ -21,4 +22,4 @@
           "Content-Type"  "text/html"
           "Cache-Control" "no-cache, no-store"
         })
-    { :port 5000 }))
+    { :port (or (env :port) 5000) }))
