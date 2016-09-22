fc53826bc2c20f090f2423a0edeabd814b3a164c
Chris <netshade@gmail.com>
2016-09-22 12:47:47 -0400

    keep command running while application is alive

--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 nohup lein with-profile production trampoline run -m yetifactory.core 2>&1 >/var/log/application.log &
 echo "${!}" > application.pid
+while kill -0 $(<application.pid)
+do
+  sleep 30
+done
 exit 0
 
