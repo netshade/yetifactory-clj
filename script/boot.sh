b2a96b7abe217ebca969ce6fcef635f67d11ebac
Chris <netshade@gmail.com>
2016-09-23 11:30:30 -0400

    manually set host identifier to task arn

--- script/boot.sh
+++ script/boot.sh
@@ -3,6 +3,7 @@ if [ -e "/tmp/ENV" ]
 then
   source /tmp/ENV
 fi
+export AWS_HOST_IDENTIFIER="$(curl $(ip neighbor | head -n 1 | awk '{print $1}'):51678/v1/tasks?dockerid=$(head -n 1 /proc/self/cgroup | awk -F':' '{print $3}' | sed 's/\/docker\///') | grep -oE 'arn:[^\"]+')"
 /etc/init.d/codedeploy-agent restart
 while [ ! -f /tmp/STOP ]
 do
