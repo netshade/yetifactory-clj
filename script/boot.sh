06b242ce96a7ac0c0970e09e195084441ee5e3f3
Chris <netshade@gmail.com>
2016-09-24 11:27:31 -0400

    fix boot script exec, also ensure AWS_HOST_IDENTIFIER is set

--- script/boot.sh
+++ script/boot.sh
@@ -13,16 +13,17 @@ aws iam create-access-key --user-name ${IAM_USER_NAME} > /tmp/CREDENTIALS
 ACCESS_KEY_ID=$(grep AccessKeyId /tmp/credentials | awk -F': ' '{print $2}' | sed 's/[\",]//g')
 SECRET_ACCESS_KEY=$(grep SecretAccessKey /tmp/credentials | awk -F': ' '{print $2}' | sed 's/[\",]//g')
 ARN=$(aws iam get-user --user-name deleteme --query "User.Arn" --output text)
+export AWS_HOST_IDENTIFIER="${ARN}"
 aws iam add-user-to-group --user-name ${IAM_USER_NAME} --group-name ${DEPLOY_GROUP}
 aws deploy register-on-premises-instance --instance-name ${INSTANCE_NAME} --iam-user-arn ${ARN} --region ${AWS_REGION}
 aws deploy add-tags-to-on-premises-instances --instance-names ${INSTANCE_NAME} --tags Key=${DEPLOY_TAG},Value=${DEPLOY_TAG_VALUE} --region ${AWS_REGION}
-cat <<EOF >/etc/rc6.d/K99_remove_iam_user
+cat <<EOF >/etc/rc6.d/K99remove_iam_user
 aws deploy deregister-on-premises-instance --instance-name ${INSTANCE_NAME} --region ${AWS_REGION}
 aws iam remove-user-from-group --user-name ${IAM_USER_NAME} --group-name ${DEPLOY_GROUP}
 aws iam delete-access-key --user-name ${IAM_USER_NAME} --access-key-key-id ${ACCESS_KEY_ID}
 aws iam delete-user --user-name ${IAM_USER_NAME}
 EOF
-chmod +x /etc/rc6.d/k99_remove_iam_user
+chmod +x /etc/rc6.d/K99remove_iam_user
 mkdir -p /etc/codedeploy-agent/conf
 cat <<EOF >/etc/codedeploy-agent/conf/codedeploy.onpremises.yml
 ---
