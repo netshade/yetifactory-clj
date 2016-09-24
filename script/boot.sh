f39642272dbc0a1ef91c1d4d8c278825d94d6384
Chris <netshade@gmail.com>
2016-09-24 09:47:27 -0400

    correct config file name

--- script/boot.sh
+++ script/boot.sh
@@ -5,10 +5,10 @@ then
 fi
 export TASK_ARN="$(curl $(ip neighbor | head -n 1 | awk '{print $1}'):51678/v1/tasks?dockerid=$(head -n 1 /proc/self/cgroup | awk -F':' '{print $3}' | sed 's/\/docker\///') | grep -oE 'arn:[^\"]+')"
 mkdir -p /etc/codedeploy-agent/conf
-cat <<EOF > /etc/codedeploy-agent/conf
+cat <<EOF >/etc/codedeploy-agent/conf/codedeploy.onpremises.yml
 ---
-aws_access_key_id: 
-aws_secret_access_key: 
+aws_access_key_id:
+aws_secret_access_key:
 iam_user_arn: ${TASK_ARN}
 region: us-east-1
 EOF
