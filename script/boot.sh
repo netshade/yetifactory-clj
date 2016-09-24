31f3a55ba9ff0f996afb2b6f81f76813f7b183bf
Chris <netshade@gmail.com>
2016-09-24 09:58:01 -0400

    also pass through credentials

--- script/boot.sh
+++ script/boot.sh
@@ -3,14 +3,15 @@ if [ -e "/tmp/ENV" ]
 then
   source /tmp/ENV
 fi
+sleep 10
 export TASK_ARN="$(curl $(ip neighbor | head -n 1 | awk '{print $1}'):51678/v1/tasks?dockerid=$(head -n 1 /proc/self/cgroup | awk -F':' '{print $3}' | sed 's/\/docker\///') | grep -oE 'arn:[^\"]+')"
 mkdir -p /etc/codedeploy-agent/conf
 cat <<EOF >/etc/codedeploy-agent/conf/codedeploy.onpremises.yml
 ---
-aws_access_key_id:
-aws_secret_access_key:
+aws_access_key_id: ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key: ${AWS_SECRET_ACCESS_KEY}
 iam_user_arn: ${TASK_ARN}
-region: us-east-1
+region: ${AWS_REGION}
 EOF
 /etc/init.d/codedeploy-agent restart
 while [ ! -f /tmp/STOP ]
