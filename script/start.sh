95d4fe8e4e9c08108aff7c364a8704fa925b6e71
Chris <netshade@gmail.com>
2016-09-24 13:29:46 -0400

    silence all output on start

--- script/start.sh
+++ script/start.sh
@@ -4,6 +4,6 @@ then
   source /tmp/ENV
 fi
 cd /app
-nohup lein with-profile production trampoline run -m yetifactory.core 2>&1 >/var/log/application.log &
+nohup lein with-profile production trampoline run -m yetifactory.core > /dev/null 2> /dev/null < /dev/null &
 echo "${!}" > application.pid
 exit 0
