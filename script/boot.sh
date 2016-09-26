9e131f8765fe70e5537bc1ffeabf3bc179649913
Chris <netshade@gmail.com>
2016-09-26 13:57:34 -0400

    boot just runs process

--- script/boot.sh
+++ script/boot.sh
@@ -3,39 +3,6 @@ if [ -e "/tmp/ENV" ]
 then
   source /tmp/ENV
 fi
-DEPLOY_GROUP="deployers"
-DEPLOY_TAG="role"
-DEPLOY_TAG_VALUE="web"
-IAM_USER_NAME="$(hostname)"
-INSTANCE_NAME="docker-${IAM_USER_NAME}"
-aws iam create-user --user-name ${IAM_USER_NAME}
-aws iam create-access-key --user-name ${IAM_USER_NAME} > /tmp/CREDENTIALS
-ACCESS_KEY_ID=$(grep AccessKeyId /tmp/CREDENTIALS | awk -F': ' '{print $2}' | sed 's/[\", ]//g')
-SECRET_ACCESS_KEY=$(grep SecretAccessKey /tmp/CREDENTIALS | awk -F': ' '{print $2}' | sed 's/[\", ]//g')
-ARN=$(aws iam get-user --user-name ${IAM_USER_NAME} --query "User.Arn" --output text)
-export AWS_HOST_IDENTIFIER="${ARN}"
-aws iam add-user-to-group --user-name ${IAM_USER_NAME} --group-name ${DEPLOY_GROUP}
-aws deploy register-on-premises-instance --instance-name ${INSTANCE_NAME} --iam-user-arn ${ARN} --region ${AWS_REGION}
-aws deploy add-tags-to-on-premises-instances --instance-names ${INSTANCE_NAME} --tags Key=${DEPLOY_TAG},Value=${DEPLOY_TAG_VALUE} --region ${AWS_REGION}
-function clean_up {
-  aws deploy deregister-on-premises-instance --instance-name ${INSTANCE_NAME} --region ${AWS_REGION}
-  aws iam remove-user-from-group --user-name ${IAM_USER_NAME} --group-name ${DEPLOY_GROUP}
-  aws iam delete-access-key --user-name ${IAM_USER_NAME} --access-key-id ${ACCESS_KEY_ID}
-  aws iam delete-user --user-name ${IAM_USER_NAME}
-	exit
-}
-trap clean_up SIGHUP SIGINT SIGTERM SIGKILL
-mkdir -p /etc/codedeploy-agent/conf
-cat <<EOF >/etc/codedeploy-agent/conf/codedeploy.onpremises.yml
----
-aws_access_key_id: "${ACCESS_KEY_ID}"
-aws_secret_access_key: "${SECRET_ACCESS_KEY}"
-iam_user_arn: "${ARN}"
-region: "${AWS_REGION}"
-EOF
-/etc/init.d/codedeploy-agent restart
-while [ ! -f /tmp/STOP ]
-do
-  sleep 1
-done
+cd /app
+exec lein with-profile production trampoline run -m yetifactory.core 
 exit 0
